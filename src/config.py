434e87a95b7c34390d2b7bc3a1daf4a7ce5a52eb
JaneiroP <janeiroplacido1944@gamil.com>
2023-05-26 22:13:36 -0400

    Changing mysql user

--- src/config.py
+++ src/config.py
@@ -2,8 +2,8 @@ class DevelopmentConfig():
     DEBUG = True
 
     MYSQL_HOST = '127.0.0.1'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'jplacido5425192'
+    MYSQL_USER = 'janeiro'
+    MYSQL_PASSWORD = 'admin'
     MYSQL_DB = 'jornada_almuerzo_gratis'
 
 
